2bb27a00d88a39aea378bfe79b999182e5f5ccd0
Константин Лущик <viskuzi1987@gmail.com>
2023-02-22 18:13:09 +0100

    fixing linter 7

--- frontend/src/slices/messagesSlice.js
+++ frontend/src/slices/messagesSlice.js
@@ -9,16 +9,15 @@ const messagesSlice = createSlice({
   name: 'messages',
   initialState,
   reducers: {
-    setMessages: (state, { payload }) => {
-      state.messages = payload;
-    },
+    setMessages: (state, { payload }) => ({ ...state, messages: payload }),
     addMessage: (state, { payload }) => {
       state.messages.push(payload);
     },
   },
   extraReducers: (builder) => {
     builder.addCase(removeChannel, (state, { payload }) => {
-      state.messages = state.messages.filter((message) => message.channelId !== payload);
+      const newMessages = state.messages.filter((message) => message.channelId !== payload);
+      return ({ ...state, messages: newMessages });
     });
   },
 });
